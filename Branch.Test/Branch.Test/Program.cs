7ad865e44e87f5683b06531d5b780cb9a0ecd1a6
Alexxander8281 <51489903+Alexxander8281@users.noreply.github.com>
2021-11-10 15:40:11 +0300

    Added Console.Writeline

--- Branch.Test/Branch.Test/Program.cs
+++ Branch.Test/Branch.Test/Program.cs
@@ -10,7 +10,7 @@ namespace Branch.Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First feature");
+            Console.WriteLine("");
         }
     }
 }
