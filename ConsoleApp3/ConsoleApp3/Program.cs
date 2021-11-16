79bc16bafc9e7f60369bf9e8b05ef6484ff1cb33
Alexxander8281 <51489903+Alexxander8281@users.noreply.github.com>
2021-11-16 21:25:54 +0300

    Added exception

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -8,16 +8,13 @@ namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void mass( List<int> list)
         {
-            var list = new List<int>() { 1, 2, 3, 4 };
-            // var list = new List<int>();
-            //list.AddRange(new int[] { 7, 8, 9 });
             var i = 0;
             Console.WriteLine("First cycle");
             for (i = 0; i < list.Count; i++)
             {
-                Console.WriteLine(list[i]);
+                Console.WriteLine(list[9]);
             
             }
 
@@ -35,6 +32,26 @@ namespace ConsoleApp3
             }
 
             Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            var list = new List<int>() { 1, 2, 3, 4 };
+            var c = 0;
+            try
+            {
+                mass(list);
+              
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+       
+            
+            
 
         }
     }
