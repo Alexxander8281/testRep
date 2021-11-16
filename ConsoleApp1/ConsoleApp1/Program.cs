737c45068e9b0c23164402a33e7aab507574e920
Alexxander8281 <51489903+Alexxander8281@users.noreply.github.com>
2021-11-16 20:28:47 +0300

    Class created

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new user { age = 10, name = "MK" };
+            string json1 = JsonSerializer.Serialize();
+
+        }
+    }
+}
